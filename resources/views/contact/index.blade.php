96b17b7e7379fcd4ff63990118040e9eee7872ac
Hxor <4kiddie04@gmail.com>
2017-11-07 14:00:31 +0700

    add delete function with sweetalert

---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,256 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="A fully featured admin theme which can be used to build CRM, CMS, etc.">
+    <meta name="author" content="Coderthemes">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <link rel="shortcut icon" href="{{ url('/') }}/assets/images/favicon_1.ico">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Chart C3 -->
+    <link href="{{ url('/') }}/assets/plugins/c3/c3.min.css" rel="stylesheet" type="text/css"  />
+
+    <!-- DataTables -->
+    <link href="{{ url('/') }}/assets/plugins/datatables/jquery.dataTables.min.css" rel="stylesheet" type="text/css"/>
+
+    <!-- DatePicker -->
+    <link href="{{ url('/') }}/assets/plugins/bootstrap-datepicker/css/bootstrap-datepicker.min.css" rel="stylesheet">
+
+    <!-- Sweet Alert -->
+    <link href="{{ url('/') }}/assets/plugins/sweet-alert2/sweetalert2.min.css" rel="stylesheet" type="text/css">
+
+    <link href="{{ url('/') }}/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="{{ url('/') }}/assets/css/core.css" rel="stylesheet" type="text/css" />
+    <link href="{{ url('/') }}/assets/css/components.css" rel="stylesheet" type="text/css" />
+    <link href="{{ url('/') }}/assets/css/icons.css" rel="stylesheet" type="text/css" />
+    <link href="{{ url('/') }}/assets/css/pages.css" rel="stylesheet" type="text/css" />
+    <link href="{{ url('/') }}/assets/css/menu.css" rel="stylesheet" type="text/css" />
+    <link href="{{ url('/') }}/assets/css/responsive.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <link href="{{ url('/') }}/assets/css/style.css" rel="stylesheet" type="text/css" />
+    <script src="{{ url('/') }}/assets/js/modernizr.min.js"></script>
+
+</head>
+
+
+<body>
+
+
+<!-- Navigation Bar-->
+<header id="topnav">
+    <div class="topbar-main">
+        <div class="container">
+
+            <!-- Logo container-->
+            <div class="logo">
+                <a href="index.html" class="logo">
+                    <span>Ub<i class="md md-album"></i>ld</span>
+                </a>
+            </div>
+            <!-- End Logo container-->
+
+
+            <div class="menu-extras">
+
+                <ul class="nav navbar-nav navbar-right pull-right">
+                    @if (Auth::guest())
+                        <li><a href="{{ route('login') }}" class="nav-link waves-effect waves-light nav-user">Login</a></li>
+                        <li><a href="{{ route('register') }}" class="nav-link waves-effect waves-light nav-user">Register</a></li>
+                    @else
+                        <li class="navbar-c-items">
+                            <form role="search" class="navbar-left app-search pull-left hidden-xs">
+                                <input type="text" placeholder="Search..." class="form-control">
+                                <a href=""><i class="fa fa-search"></i></a>
+                            </form>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li>
+                                    <a href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        Logout
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        {{ csrf_field() }}
+                                    </form>
+                                </li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+                <div class="menu-item">
+                    <!-- Mobile menu toggle-->
+                    <a class="navbar-toggle">
+                        <div class="lines">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </a>
+                    <!-- End mobile menu toggle-->
+                </div>
+            </div>
+
+        </div>
+    </div>
+</header>
+<!-- End Navigation Bar-->
+
+
+<div class="wrapper">
+    <div class="container">
+
+        <!-- Page-Title -->
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="btn-group pull-right m-t-15">
+                    <button type="button" class="btn btn-default dropdown-toggle waves-effect waves-light" data-toggle="dropdown" aria-expanded="false">Settings <span class="m-l-5"><i class="fa fa-cog"></i></span></button>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </div>
+                <h4 class="page-title">Dashboard</h4>
+                <p class="text-muted page-title-alt">Welcome to Ubold admin panel !</p>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="card-box table-responsive">
+                    <h4 class="m-t-0 header-title"><b>Default Example</b></h4>
+                    <p class="text-muted font-13 m-b-30">
+                       <a href="{{ route('contact.create') }}" class="btn btn-primary">Create Contact</a>
+                    </p>
+
+                    <table id="datatable" class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th></th>
+                        </tr>
+                        </thead>
+
+
+                        <tbody>
+                        @if (count($contacts) > 0)
+                            @foreach ($contacts as $contact)
+                                <tr>
+                                    <td>{{ $contact->id }}</td>
+                                    <td>{{ $contact->name }}</td>
+                                    <td>{{ $contact->email }}</td>
+                                    <td>
+                                        <a href="#" class="btn btn-info btn-xs"><i class="glyphicon glyphicon-eye-open"></i> Show</a>
+                                        <a class="btn btn-primary btn-xs"><i class="glyphicon glyphicon-edit"></i> Edit</a>
+                                        <form method="post" action="{{ route('contact.destroy', $contact->id) }}">
+                                            {{ method_field('DELETE') }} {{ csrf_field() }}
+                                            <button type="submit" class="btn btn-danger btn-xs js-submit-confirm"><i class="glyphicon glyphicon-trash"></i> Delete</button>
+                                        </form>
+                                        
+                                    </td>
+                                </tr>
+                            @endforeach
+                            @else
+                            <p>Tidak ada data</p>
+                        @endif
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- End row Datatable -->
+
+        <!-- Footer -->
+        <footer class="footer text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        © 2017. All rights reserved.
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End Footer -->
+
+    </div>
+</div>
+
+
+
+<!-- jQuery  -->
+<script src="{{ url('/') }}/assets/js/jquery.min.js"></script>
+<script src="{{ url('/') }}/assets/js/bootstrap.min.js"></script>
+<script src="{{ url('/') }}/assets/js/detect.js"></script>
+<script src="{{ url('/') }}/assets/js/fastclick.js"></script>
+
+<script src="{{ url('/') }}/assets/js/jquery.slimscroll.js"></script>
+<script src="{{ url('/') }}/assets/js/jquery.blockUI.js"></script>
+<script src="{{ url('/') }}/assets/js/waves.js"></script>
+<script src="{{ url('/') }}/assets/js/wow.min.js"></script>
+<script src="{{ url('/') }}/assets/js/jquery.nicescroll.js"></script>
+<script src="{{ url('/') }}/assets/js/jquery.scrollTo.min.js"></script>
+
+<script src="{{ url('/') }}/assets/js/jquery.core.js"></script>
+<script src="{{ url('/') }}/assets/js/jquery.app.js"></script>
+
+<!-- Datatables -->
+<script src="{{ url('/') }}/assets/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="{{ url('/') }}/assets/plugins/datatables/dataTables.bootstrap.js"></script>
+<script src="{{ url('/') }}/assets/pages/datatables.init.js"></script>
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#datatable').dataTable();
+    });
+</script>
+
+<!-- Sweet-Alert 2 -->
+<script src="{{ url('/') }}/assets/plugins/sweet-alert2/sweetalert2.min.js"></script>
+<script src="{{ url('/') }}/assets/pages/jquery.sweet-alert2.init.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        /*sweetalert confirm*/
+        $(document.body).on('click', '.js-submit-confirm', function (event) {
+            event.preventDefault();
+            var $form = $(this).closest('form');
+            var $el = $(this);
+            var text = $el.data('confirm-message') ? $el.data('confirm-message') : 'You will not be able to recover this imaginary file!';
+            swal({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                type: 'warning',
+                showCancelButton: true,
+                cancelButtonColor: '#d33',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Yes, delete it!'
+            }).then(function () {
+                $form.submit()
+            })
+        });
+    });
+</script>
+
+</body>
+</html>
